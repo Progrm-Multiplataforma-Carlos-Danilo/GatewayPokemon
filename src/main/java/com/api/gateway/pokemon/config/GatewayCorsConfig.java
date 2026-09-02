d3c972003879493e705d6867f834cc140648481b
DaniloSsoares <danilo.Ssoares201@gmail.com>
2026-09-02 17:35:43 -0300

    fix: correção gateway

--- src/main/java/com/api/gateway/pokemon/config/GatewayCorsConfig.java
+++ src/main/java/com/api/gateway/pokemon/config/GatewayCorsConfig.java
@@ -1,14 +1,15 @@
 package com.api.gateway.pokemon.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
+@Configuration
 public class GatewayCorsConfig {
-    
 
     private final CorsProperties properties;
 
@@ -19,9 +20,12 @@ public class GatewayCorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration cors = new CorsConfiguration();
-        cors.setAllowedOrigins(properties.getUrl());
-        cors.setAllowedOrigins(List.of("http://localhost:8081"));
-        cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
+        if (properties != null && properties.getUrl() != null && !properties.getUrl().isEmpty()) {
+            cors.setAllowedOriginPatterns(properties.getUrl());
+        } else {
+            cors.setAllowedOriginPatterns(List.of("*"));
+        }
+        cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"));
         cors.setAllowedHeaders(List.of("*"));
         cors.setExposedHeaders(List.of("Authorization"));
         cors.setAllowCredentials(true);
